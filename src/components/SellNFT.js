d32a27436e86c86b2c505b87f40c99af1954ad1a
Vikas Yadav <yadavikas13@gmail.com>
2023-02-15 22:03:22 +0530

    author name

--- src/components/SellNFT.js
+++ src/components/SellNFT.js
@@ -93,11 +93,11 @@ export default function SellNFT () {
             <form className="bg-white shadow-md rounded px-8 pt-4 pb-8 mb-4">
             <h3 className="text-center font-bold text-purple-500 mb-8">Upload your NFT to the marketplace</h3>
                 <div className="mb-4">
-                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="name">NFT Name</label>
+                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="name">Author Name</label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Axie#4563" onChange={e => updateFormParams({...formParams, name: e.target.value})} value={formParams.name}></input>
                 </div>
                 <div className="mb-6">
-                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="description">NFT Description</label>
+                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="description">Book Description</label>
                     <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" cols="40" rows="5" id="description" type="text" placeholder="Axie Infinity Collection" value={formParams.description} onChange={e => updateFormParams({...formParams, description: e.target.value})}></textarea>
                 </div>
                 <div className="mb-6">
